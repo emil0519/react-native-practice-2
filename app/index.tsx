45c849d171f68c3759d18e8e348e7368425aa724
Emil Lau <emil.lau@xterraplanet.com>
2025-05-07 22:25:50 +0800

    Set up React native pager view

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,19 @@
-import { verifyInstallation } from "nativewind";
+import React from "react";
 import { Text, View } from "react-native";
+import PagerView from "react-native-pager-view";
 import "../global.css";
 
-export default function Index() {
-  verifyInstallation();
-
+const MyPager = () => {
   return (
-    <View className="flex justify-center items-center h-full">
-      <Text className="text-red-500">
-        Edit app/index.tsx to edit this screen.
-      </Text>
-    </View>
+    <PagerView style={{ flex: 1 }} initialPage={0}>
+      <View key="1">
+        <Text className="text-red-500">First page</Text>
+      </View>
+      <View key="2">
+        <Text>Second page</Text>
+      </View>
+    </PagerView>
   );
-}
+};
+
+export default MyPager;
