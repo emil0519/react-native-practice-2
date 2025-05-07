fbbdeb21418490cb449dd5786055ae291d9bad31
Emil Lau <emil.lau@xterraplanet.com>
2025-05-07 22:05:22 +0800

    Set up native wind

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,15 @@
+import { verifyInstallation } from "nativewind";
 import { Text, View } from "react-native";
+import "../global.css";
 
 export default function Index() {
+  verifyInstallation();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex justify-center items-center h-full">
+      <Text className="text-red-500">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
